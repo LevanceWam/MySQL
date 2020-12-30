28630932710ed98dcaabea58fc5bc476158be940
LevanceWam <lkwamley@fullsail.edu>
2020-12-30 15:10:49 -0500

    Learned more about the null operator

--- RDFST/NULLOperator/NULLoperater.sql
+++ RDFST/NULLOperator/NULLoperater.sql
@@ -1,4 +1,16 @@
 --  this returns customers that dont have a phone 
 SELECT *
 FROM customers
-WHERE phone IS NULL
+WHERE phone IS NULL;
+
+-- returns the customers with a phone number 
+-- opposite of the above statement 
+SELECT *
+FROM customers
+WHERE phone IS NOT NULL;
+
+-- Exercise: 
+-- Get the orders that are not shipped
+SELECT *
+FROM orders
+WHERE shipped_date IS NULL;
