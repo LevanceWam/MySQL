b0aa9116581c1d44156a952b0758d7d074dd9742
LevanceWam <lkwamley@fullsail.edu>
2021-01-01 12:16:08 -0500

    Learned about inner joins in sql

--- RSFMT/InnerJoins/InnerJoins.sql
+++ RSFMT/InnerJoins/InnerJoins.sql
@@ -32,4 +32,4 @@ JOIN customers c
 SELECT order_id, p.product_id, name, quantity, oi.unit_price
 FROM order_items oi
 JOIN products p
-	ON oi.product_id = p.product_id
+	ON oi.product_id = p.product_id;
