c7e8a86de8efb555e9ef91cc7b5be15eb919c76f
LevanceWam <lkwamley@fullsail.edu>
2021-01-07 17:23:12 -0500

    learned how to updated a single row in sql

--- IUDD/UpdatingASingleRow/USR.sql
+++ IUDD/UpdatingASingleRow/USR.sql
@@ -5,4 +5,19 @@ USE sql_invoicing;
 -- then we use WHERE to apply the update to the records
 UPDATE invoices 
 SET payment_total = 10, payment_date = '2019-03-01'
-WHERE invoice_id = 1
+WHERE invoice_id = 1;
+
+-- undoing the update to invoice 
+-- since the default 
+UPDATE invoices 
+SET payment_total = DEFAULT, payment_date = NULL
+WHERE invoice_id = 1;
+
+-- updating the 3rd invoice
+-- calculating a percentage from another column
+-- since the customer paid on the due date we are going to use that value inside of the column
+UPDATE invoices 
+SET payment_total = 
+    invoice_total * 0.5, 
+    payment_date = due_date
+WHERE invoice_id = 3;
